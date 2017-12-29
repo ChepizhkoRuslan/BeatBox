d4626db1e2aa4f4a0c8942875013697f37ffc8f3
Ruslan Chepizhko <chepizhko.ruslan@gmail.com>
2017-12-29 08:47:57 +0200

    init BeatBox

---
+++ app/src/main/java/com/chepizhko/beatbox/BeatBoxFragment.java
@@ -0,0 +1,11 @@
+package com.chepizhko.beatbox;
+
+import android.support.v4.app.Fragment;
+
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
